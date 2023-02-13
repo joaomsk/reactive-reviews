0dcc9460fe10996a2f8642ba21bfff9674af6b96
João Meleski Dier <joao.dier@finplace.com.br>
2023-02-13 10:31:37 -0300

    remove @SpringBootTest from Service unit test

--- src/test/java/com/joaomeleski/reactivereviews/service/ReviewServiceTest.java
+++ src/test/java/com/joaomeleski/reactivereviews/service/ReviewServiceTest.java
@@ -12,14 +12,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ReviewServiceTest {
 
